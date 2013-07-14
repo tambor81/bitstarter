d144da44cf29adff3c71717fe4ad96ea05a99ce6
Luis Bernabe <luis_martinb@hotmail.com>
2013-07-14 21:09:16 +0000

    Updated to read index.html

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+
+  var buff = fs.readFileSync("index.html",  "utf8");
+  response.send(buff);
 });
 
 var port = process.env.PORT || 5000;
